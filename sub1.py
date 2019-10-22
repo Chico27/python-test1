6b95ed35e0cab789348229f8da0683a4dc681d81
chico27 <masamasa.987654321@gmail.com>
2019-10-22 10:54:59 +0900

    new

--- sub1.py
+++ sub1.py
@@ -3,5 +3,14 @@ import sub2
 
 def sub1func(testString):
     testString += '\nSub1 passed'
-    return sub2.sub2func(testString)
+
+    import inspect
+    print('')
+    print('呼び出し元のファイル')
+    print(inspect.currentframe().f_back.f_code.co_filename)
+    print('呼び出し元のライン')
+    print(inspect.currentframe().f_back.f_lineno)
+
+    return testString
+    # return sub2.sub2func(testString)
 
